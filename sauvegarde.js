f71d1fc15c2a35cbf507954504bbb4bd903ec228
Romain Valls <romain.valls95@gmail.com>
2023-04-23 17:23:48 +0200

    2 levels created. Can create multiple with this base. User-interface created displaying Player 1. Start button makes evevrything appears.

---
+++ sauvegarde.js
@@ -0,0 +1,196 @@
+const startButton = document.getElementById("start")
+const gridElement = document.querySelector(".grid")
+const allTheCells = []
+let playerPosition = 49;
+let target
+let target2
+
+startButton.addEventListener("click", startTheGame);
+
+
+function startTheGame() {
+	for (let i = 0; i < 50; i++) {
+		createACell()
+	}
+	// target = Math.floor(Math.random() * allTheCells.length - 1) + 1
+	// target2 = Math.floor(Math.random() * allTheCells.length - 1) + 1
+    target = 25;
+    target2 = 12;
+	displayPlayer()
+	displayTarget()
+    displayTarget2()
+}
+
+function displayTarget() {
+	allTheCells[target].classList.add("target")
+}
+
+// Je rajoute une deuxième target
+
+function displayTarget2() {
+	allTheCells[target2].classList.add("targetTwo")    // Je crée une autre classe pour pouvoir display deux messages différents en fonction de qui le joueur rencontre
+}
+
+function displayExit() {
+    let exit = 2;
+	allTheCells[exit].classList.add("exit") // add exit class
+}
+
+function displayPlayer() {
+	allTheCells[playerPosition].classList.add("player")
+}
+
+
+function createACell() {
+	const div = document.createElement("div")
+	div.classList.add("cell")
+	gridElement.append(div)
+	allTheCells.push(div)
+}
+
+function hidePlayer() {
+	allTheCells[playerPosition].classList.remove("player")
+}
+
+function move(direction) {
+	
+    hidePlayer();
+
+
+	if (direction === "right") {
+		if ((playerPosition + 1) % 10 === 0) {
+			return displayPlayer()
+		}
+		playerPosition += 1
+	}
+
+
+	if (direction === "left") {
+		if (playerPosition % 10 === 0) {
+			return displayPlayer()
+		}
+		playerPosition -= 1
+	}
+
+
+	if (direction === "up") {
+		if (playerPosition < 10) {
+			return displayPlayer()
+		}
+		playerPosition -= 10
+	}
+
+
+	if (direction === "down") {
+		if (playerPosition >= 90) {
+			return displayPlayer()
+		}
+		playerPosition += 10
+	}
+
+
+	displayPlayer()
+
+
+    // Dans ce If statement, je veux rajouter avant le remove, un dialog a choix multiple 
+
+	if (allTheCells[playerPosition].classList.contains("target")) {   //je veux que les pnj soient des objets colision. 
+        
+        
+        // function createContainer() {
+        //     const container = document.createElement("div");         
+        //     body.appendChild(container);
+        // }
+        // createContainer();
+
+            function createDialog() {
+            const dialogDiv = document.createElement("div");
+            dialogDiv.id = "dialog1";
+            const dialogText = document.createElement("p");
+            dialogText.textContent = "This is your first challenge adventurer ! bakrnfanfnazefnazjnfvlaernvlanevn,arnvazrjnlvanerj";
+            const button1 = document.createElement("button");
+            button1.id = "button1";
+            button1.textContent = "Solution 1";
+            const button2 = document.createElement("button");
+            button2.id = "button2";
+            button2.textContent = "Solution 2";
+
+            dialogDiv.appendChild(dialogText);
+            dialogDiv.appendChild(button1);
+            dialogDiv.appendChild(button2);
+            document.body.appendChild(dialogDiv);
+        }
+
+        createDialog();
+        allTheCells[playerPosition].classList.remove("target"); 
+
+        // Display exit method 
+
+        if (!allTheCells.some(cell => cell.classList.contains("target"))) {
+            // both targets have been removed, display exit
+            displayExit();
+        }
+
+	}
+
+
+	if (allTheCells[playerPosition].classList.contains("targetTwo")) {    //ça va changer, je veux que les pnj soient des objets colision. 
+
+        function createDialog() {
+            const dialogDivTwo = document.createElement("div");
+            dialogDivTwo.id = "dialog2";
+            const dialogTextTwo = document.createElement("p");
+            dialogTextTwo.textContent = "This is your second challenge adventurer !";
+            const button3 = document.createElement("button");
+            button1.id = "button3";
+            button3.textContent = "Solution 3";
+            const button4 = document.createElement("button");
+            button4.id = "button4";
+            button4.textContent = "Solution 4";
+
+            dialogDivTwo.appendChild(dialogTextTwo);
+            dialogDivTwo.appendChild(button3);
+            dialogDivTwo.appendChild(button4);
+            document.body.appendChild(dialogDivTwo);
+        }
+
+        createDialog();
+		
+		allTheCells[playerPosition].classList.remove("targetTwo"); 
+
+        // Display exit method 
+
+        if (!allTheCells.some(cell => cell.classList.contains("target") || cell.classList.contains("targetTwo"))) {
+            // both targets have been removed, display exit
+            displayExit();
+	        }
+        }
+}
+
+
+
+// Je veux que si il n'y a plus de target dans mon grid, une nouvelle target d'une autre couleur est créée, ce sera la porte de sortie du niveau 
+
+// function createExit() {
+//     for (let i=0 ; i < 50 ; i++) {
+//         if 
+//     }
+// }
+
+document.addEventListener("keydown", (event) => {
+	console.log(event.key)
+	switch (event.key) {
+		case "ArrowRight":
+			move("right")
+			break
+		case "ArrowLeft":
+			move("left")
+			break
+		case "ArrowDown":
+			move("down")
+			break
+		case "ArrowUp":
+			move("up")
+			break
+	}
+})
--- script.js
+++ script.js
@@ -1,37 +1,77 @@
 const startButton = document.getElementById("start")
 const gridElement = document.querySelector(".grid")
-const allTheCells = []
-let playerPosition = 49;
+const allTheCells = [];
+let playerPosition = 90;
+const userInterface = document.querySelector('.user-interface');
 let target
 let target2
-let exit
-startButton.addEventListener("click", startTheGame)
+let level = 1; 
+startButton.addEventListener("click", startTheGame);
 
 
 function startTheGame() {
-	for (let i = 0; i < 50; i++) {
+    userInterface.style.display = "block";
+    userInterface.style.backgroundColor = "red";
+    
+    if (level === 1){
+        
+	for (let i = 0; i < 100; i++) {
 		createACell()
 	}
-	target = Math.floor(Math.random() * allTheCells.length - 1) + 1
-	target2 = Math.floor(Math.random() * allTheCells.length - 1) + 1
+	// target = Math.floor(Math.random() * allTheCells.length - 1) + 1
+	// target2 = Math.floor(Math.random() * allTheCells.length - 1) + 1
+    target = 25;
+    target2 = 12;
 	displayPlayer()
 	displayTarget()
     displayTarget2()
+    }
+
+
+    if (level === 2){
+        clearGrid();
+	for (let i = 0; i < 100; i++) {
+		createACell2();
+	}
+	
+    target = 27;
+    target2 = 19;
+	displayPlayer2()
+	displayTarget3()
+    displayTarget4()
+    }
+
+
+    if (level === 3){
+	for (let i = 0; i < 100; i++) {
+		createACell3();
+	}
+
+    target = 25;
+    target2 = 12;
+	displayPlayer3()
+	displayTarget5()
+    displayTarget6()
+    }
+}
+
+function displayPlayer() {
+    allTheCells[playerPosition].classList.add("player")
 }
 
 function displayTarget() {
 	allTheCells[target].classList.add("target")
 }
 
-// Je rajoute une deuxième target
-
 function displayTarget2() {
 	allTheCells[target2].classList.add("targetTwo")    // Je crée une autre classe pour pouvoir display deux messages différents en fonction de qui le joueur rencontre
 }
 
-function displayPlayer() {
-	allTheCells[playerPosition].classList.add("player")
+function displayExit() {
+    let exit = 2;
+	allTheCells[exit].classList.add("exit") // add exit class
 }
+
 
 
 function createACell() {
@@ -41,13 +81,28 @@ function createACell() {
 	allTheCells.push(div)
 }
 
+
+function clearGrid() {
+    // Remove all cells from the grid
+    while (gridElement.firstChild) {
+      gridElement.removeChild(gridElement.firstChild);
+    }
+    // Clear the allTheCells array
+    allTheCells.length = 0;
+  }
+
+
 function hidePlayer() {
 	allTheCells[playerPosition].classList.remove("player")
 }
 
+// Move function
+
 function move(direction) {
 	
-    hidePlayer()
+    hidePlayer();
+
+
 	if (direction === "right") {
 		if ((playerPosition + 1) % 10 === 0) {
 			return displayPlayer()
@@ -55,30 +110,37 @@ function move(direction) {
 		playerPosition += 1
 	}
 
+
 	if (direction === "left") {
 		if (playerPosition % 10 === 0) {
 			return displayPlayer()
 		}
 		playerPosition -= 1
 	}
+
+
 	if (direction === "up") {
 		if (playerPosition < 10) {
 			return displayPlayer()
 		}
 		playerPosition -= 10
 	}
+
+
 	if (direction === "down") {
 		if (playerPosition >= 90) {
 			return displayPlayer()
 		}
 		playerPosition += 10
 	}
+
+
 	displayPlayer()
 
 
     // Dans ce If statement, je veux rajouter avant le remove, un dialog a choix multiple 
 
-	if (allTheCells[playerPosition].classList.contains("target")) {   //ça va changer, je veux que les pnj soient des objets colision. 
+	if (allTheCells[playerPosition].classList.contains("target")) {   //je veux que les pnj soient des objets colision. 
         
         
         // function createContainer() {
@@ -87,32 +149,40 @@ function move(direction) {
         // }
         // createContainer();
 
-
-
-
-        function createDialog() {
+            function createDialog() {
             const dialogDiv = document.createElement("div");
             dialogDiv.id = "dialog1";
             const dialogText = document.createElement("p");
-            dialogText.textContent = "This is your first challenge adventurer ! bakrnfanfnazefnazjnfvlaernvlanevn,arnvazrjnlvanerj";
+            dialogText.textContent = "This is your first challenge adventurer !";
             const button1 = document.createElement("button");
             button1.id = "button1";
             button1.textContent = "Solution 1";
+            button1.addEventListener("click", () => {
+                dialogDiv.remove();
+            });
             const button2 = document.createElement("button");
             button2.id = "button2";
             button2.textContent = "Solution 2";
+            button2.addEventListener("click", () => {
+                dialogDiv.remove();
+            });
 
             dialogDiv.appendChild(dialogText);
             dialogDiv.appendChild(button1);
             dialogDiv.appendChild(button2);
             document.body.appendChild(dialogDiv);
         }
-        allTheCells[playerPosition].classList.remove("target"); 
-        createDialog()
 
-		// allTheCells[playerPosition].classList.remove("target") // 
+        createDialog();
+        allTheCells[playerPosition].classList.remove("target"); 
+
+        // Display exit method 
 
 	}
+
+    
+
+
 	if (allTheCells[playerPosition].classList.contains("targetTwo")) {    //ça va changer, je veux que les pnj soient des objets colision. 
 
         function createDialog() {
@@ -121,11 +191,17 @@ function move(direction) {
             const dialogTextTwo = document.createElement("p");
             dialogTextTwo.textContent = "This is your second challenge adventurer !";
             const button3 = document.createElement("button");
-            button1.id = "button3";
+            button3.id = "button3";
             button3.textContent = "Solution 3";
+            button3.addEventListener("click", () => {
+                dialogDivTwo.remove();
+            });
             const button4 = document.createElement("button");
             button4.id = "button4";
             button4.textContent = "Solution 4";
+            button4.addEventListener("click", () => {
+                dialogDivTwo.remove();  
+            });
 
             dialogDivTwo.appendChild(dialogTextTwo);
             dialogDivTwo.appendChild(button3);
@@ -133,12 +209,197 @@ function move(direction) {
             document.body.appendChild(dialogDivTwo);
         }
 
-        createDialog()
-
+        createDialog();
 		
 		allTheCells[playerPosition].classList.remove("targetTwo"); 
 
+        // Display exit method 
+
+        if (!allTheCells.some(cell => cell.classList.contains("target") || cell.classList.contains("targetTwo"))) {
+            // both targets have been removed, display exit
+            displayExit();
+	        }
+            
+        } 
+
+        else if (allTheCells[playerPosition].classList.contains("exit")) {
+            hidePlayer();
+            level = 2; 
+            playerPosition = 99;
+            startTheGame();
+        }
+        
+}
+
+
+
+
+
+
+
+// ATTENTION, CETTE PARTIE POUR LE LEVEL 2
+
+
+
+
+
+
+function displayTarget3() {
+	allTheCells[target].classList.add("targetThree")
+}
+
+function displayTarget4() {
+	allTheCells[target2].classList.add("targetFour")    // Je crée une autre classe pour pouvoir display deux messages différents en fonction de qui le joueur rencontre
+}
+
+function displayExit2() {
+    let exit = 2;
+	allTheCells[exit].classList.add("exit") // add exit class
+}
+
+function displayPlayer2() {
+	allTheCells[playerPosition].classList.add("player")
+}
+
+
+function createACell2() {
+	const div = document.createElement("div")
+	div.classList.add("cell")
+	gridElement.append(div)
+	allTheCells.push(div)
+}
+
+function hidePlayer2() {
+	allTheCells[playerPosition].classList.remove("player")
+}
+
+// Move function
+
+function move2(direction) {
+	
+    hidePlayer2();
+
+
+	if (direction === "right") {
+		if ((playerPosition + 1) % 10 === 0) {
+			return displayPlayer2()
+		}
+		playerPosition += 1
 	}
+
+
+	if (direction === "left") {
+		if (playerPosition % 10 === 0) {
+			return displayPlayer2()
+		}
+		playerPosition -= 1
+	}
+
+
+	if (direction === "up") {
+		if (playerPosition < 10) {
+			return displayPlayer2()
+		}
+		playerPosition -= 10
+	}
+
+
+	if (direction === "down") {
+		if (playerPosition >= 90) {
+			return displayPlayer2()
+		}
+		playerPosition += 10
+	}
+
+
+	displayPlayer2()
+
+
+    // Dans ce If statement, je veux rajouter avant le remove, un dialog a choix multiple 
+
+	if (allTheCells[playerPosition].classList.contains("targetThree")) {   //je veux que les pnj soient des objets colision. 
+        
+        
+        // function createContainer() {
+        //     const container = document.createElement("div");         
+        //     body.appendChild(container);
+        // }
+        // createContainer();
+
+            function createDialog() {
+            const dialogDiv = document.createElement("div");
+            dialogDiv.id = "dialog1";
+            const dialogText = document.createElement("p");
+            dialogText.textContent = "This is your first challenge adventurer ! bakrnfanfnazefnazjnfvlaernvlanevn,arnvazrjnlvanerj";
+            const button1 = document.createElement("button");
+            button1.id = "button1";
+            button1.textContent = "Solution 1";
+            button1.addEventListener("click", () => {
+                dialogDiv.remove();
+            })
+            const button2 = document.createElement("button");
+            button2.id = "button2";
+            button2.textContent = "Solution 2";
+            button2.addEventListener("click", () => {
+                dialogDiv.remove();
+            })
+
+            dialogDiv.appendChild(dialogText);
+            dialogDiv.appendChild(button1);
+            dialogDiv.appendChild(button2);
+            document.body.appendChild(dialogDiv);
+        }
+
+        createDialog();
+        allTheCells[playerPosition].classList.remove("targetThree"); 
+
+        // Display exit method 
+
+        if (!allTheCells.some(cell => cell.classList.contains("targetThree"))) {
+            // both targets have been removed, display exit
+            displayExit();
+        }
+
+	}
+
+
+	if (allTheCells[playerPosition].classList.contains("targetFour")) {    //ça va changer, je veux que les pnj soient des objets colision. 
+
+        function createDialog() {
+            const dialogDivTwo = document.createElement("div");
+            dialogDivTwo.id = "dialog2";
+            const dialogTextTwo = document.createElement("p");
+            dialogTextTwo.textContent = "This is your second challenge adventurer !";
+            const button3 = document.createElement("button");
+            button3.id = "button3";
+            button3.textContent = "Solution 3";
+            button3.addEventListener("click", () => {
+                dialogDivTwo.remove();
+            });
+            const button4 = document.createElement("button");
+            button4.id = "button4";
+            button4.textContent = "Solution 4";
+            button4.addEventListener("click", () => {
+                dialogDivTwo.remove();
+            })
+
+            dialogDivTwo.appendChild(dialogTextTwo);
+            dialogDivTwo.appendChild(button3);
+            dialogDivTwo.appendChild(button4);
+            document.body.appendChild(dialogDivTwo);
+        }
+
+        createDialog();
+		
+		allTheCells[playerPosition].classList.remove("targetFour"); 
+
+        // Display exit method 
+
+        if (!allTheCells.some(cell => cell.classList.contains("targetThree") || cell.classList.contains("targetFour"))) {
+            // both targets have been removed, display exit
+            displayExit();
+	        }
+        }
 }
 
 // Je veux que si il n'y a plus de target dans mon grid, une nouvelle target d'une autre couleur est créée, ce sera la porte de sortie du niveau 
@@ -150,7 +411,9 @@ function move(direction) {
 // }
 
 document.addEventListener("keydown", (event) => {
-	console.log(event.key)
+
+    if (level === 1) {
+    console.log(event.key)
 	switch (event.key) {
 		case "ArrowRight":
 			move("right")
@@ -164,6 +427,22 @@ document.addEventListener("keydown", (event) => {
 		case "ArrowUp":
 			move("up")
 			break
-	}
+	}}
+    if (level === 2) {
+    console.log(event.key)
+	switch (event.key) {
+		case "ArrowRight":
+			move2("right")
+			break
+		case "ArrowLeft":
+			move2("left")
+			break
+		case "ArrowDown":
+			move2("down")
+			break
+		case "ArrowUp":
+			move2("up")
+			break
+	}}
 })
 
